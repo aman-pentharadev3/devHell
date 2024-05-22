79f8fab67ed3237e7a4502eb8a845cded09da6e9
Aman-pen <amans@pentharadev3.onmicrosoft.com>
2024-05-22 22:33:05 +0530

    Rewrite SSO Auth

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
-// Material-UI imports
-import Grid from "@mui/material/Grid";
+import React from "react";
+import { useIsAuthenticated } from "@azure/msal-react";
 
-// MSAL imports
-import { MsalProvider } from "@azure/msal-react";
-import { CustomNavigationClient } from "./utils/NavigationClient";
+const App = () => {
+  const isAuthenticated = useIsAuthenticated();
 
-// Sample app imports
-import { PageLayout } from "./ui-components/PageLayout";
-import { Home } from "./pages/Home";
-import { Profile } from "./pages/Profile";
-import { Logout } from "./pages/Logout";
-
-// Class-based equivalents of "Profile" component
-import { ProfileUseMsalAuthenticationHook } from "./pages/ProfileUseMsalAuthenticationHook";
-
-function App({ pca }) {
-    // The next 3 lines are optional. This is how you configure MSAL to take advantage of the router's navigate functions when MSAL redirects between pages in your app
-    const navigate = useNavigate();
-    const navigationClient = new CustomNavigationClient(navigate);
-    pca.setNavigationClient(navigationClient);
-
-    return (
-        <MsalProvider instance={pca}>
-            <PageLayout>
-                <Grid container justifyContent="center">
-                    <Pages />
-                </Grid>
-            </PageLayout>
-        </MsalProvider>
-    );
-}
-
-function Pages() {
-    return (
-        <Routes>
-            <Route path="/profile" element={<Profile />} />
-            <Route
-                path="/profileUseMsalAuthenticationHook"
-                element={<ProfileUseMsalAuthenticationHook />}
-            />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/" element={<Home />} />
-        </Routes>
-    );
-}
+  return (
+    <>
+      <p>Anyone can see this paragraph.</p>
+      {isAuthenticated && <p>At least one account is signed in!</p>}
+      {!isAuthenticated && <p>No users are signed in!</p>}
+    </>
+  );
+};
 
 export default App;
